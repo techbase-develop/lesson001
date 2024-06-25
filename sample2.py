0ca419765dcd7d2261f85d1162e263022b3c5693
Tetsuya-Ikezawa <33471203+Tetsuya-Ikezawa@users.noreply.github.com>
2024-06-25 10:34:36 +0900

    Create sample2.py

---
+++ sample2.py
@@ -0,0 +1,15 @@
+# 整数型の変数
+age = 25
+print("Age:", age)
+
+# 浮動小数点数型の変数
+height = 175.5
+print("Height:", height)
+
+# 文字列型の変数
+name = "Alice"
+print("Name:", name)
+
+# ブール型の変数
+is_student = True
+print("Is student:", is_student)
